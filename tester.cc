3bc1a518d8c5aa0bce0707c38d40c7a57b7a3604
mnto <to.nguyet.minh@gmail.com>
2016-05-24 14:39:12 +0700

    changed tester

---
+++ tester.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "priqueue.h"
+
+using namespace std;
+
+
+int mypri(const string & item)
+{
+	// return the number of letters in the word
+	string vowels = "aeiouy";
+	int sum = 0;
+	for (size_t i = 0; i < item.size(); i++) {
+		if (vowels.find(item[i]) != string::npos)
+			sum++;
+	}
+	return sum;
+}
+
+int main()
+{
+	priqueue q(mypri); //
+	q.add("onomatapiay"); //
+	q.add("hi"); //
+	q.add("banana"); //
+	q.add("ho"); //
+	while (q.size() != 0) {
+		cout << q.front() << endl;
+		q.remove();
+	}
+}
